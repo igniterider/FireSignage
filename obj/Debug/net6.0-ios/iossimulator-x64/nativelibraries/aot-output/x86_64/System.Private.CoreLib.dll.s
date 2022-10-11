d6c931a77895d3fc01c8250840c64e2673ee7547
brianjohnson <brianj@igniterider.com>
2022-10-11 11:35:24 -0400

    Great Save Point

--- obj/Debug/net6.0-ios/iossimulator-x64/nativelibraries/aot-output/x86_64/System.Private.CoreLib.dll.s
+++ obj/Debug/net6.0-ios/iossimulator-x64/nativelibraries/aot-output/x86_64/System.Private.CoreLib.dll.s
@@ -61226,7 +61226,7 @@ _mono_aot_file_info:
 	.long 0,297,8489,16681,17193,17193,17193,8
 	.long 13,45,0,0,0,0,0,0
 	.long 0,0,0
-	.byte 96,63,109,15,160,89,22,242,115,14,162,194,108,170,175,218
+	.byte 19,170,75,29,72,98,50,59,63,147,156,142,191,70,132,73
 	.globl _mono_aot_module_System_Private_CoreLib_info
 	.align 3
 _mono_aot_module_System_Private_CoreLib_info:
