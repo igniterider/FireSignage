e5f4d4b2d48c64be0d55deb85319e9f155c6c1ba
Brian Johnson <brianj@igniterider.com>
2022-11-14 14:41:12 -0500

    still working on splash screen for android. Fixed display sign and vm

--- Viewmodels/DeviceSignViewModel.cs
+++ Viewmodels/DeviceSignViewModel.cs
@@ -1,22 +1,25 @@
-﻿using Realms;
+﻿using CommunityToolkit.Mvvm.Input;
+using Realms;
 using Realms.Sync;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FireSignage.Viewmodels
 {
     public partial class DeviceSignViewModel : BaseViewModel
     {
-        private Realm _deviceissignrealm;
+        private Realm signrealm;
         string NameofPage;
-        public Command DisplaySetupCommand { get; }
+       
+        public DisplaySign getsign { get; }
 
-        public string backcolor;
+        private string backcolor;
         private string backcolor2;
-        public string displaytext;
+        private string displaytext;
         private string displaytext2;
         private string ridername;
         private string textcolor;
@@ -26,60 +29,98 @@ namespace FireSignage.Viewmodels
 
         public DeviceSignViewModel()
         {
-            DisplaySetupCommand = new Command(Watching);
+            
+            
+        }
+
+
+        public string DisplayText
+        {
+            set { SetProperty(ref displaytext, value); }
+            get { return displaytext; }
+
 
         }
 
-        private async void Watching()
+        [RelayCommand]
+        async Task Watching()
         {
             var part = App.realmApp.CurrentUser.Id;
 
-            if (_deviceissignrealm == null)
+            if (signrealm == null)
             {
 
 
                 var syncConfig = new PartitionSyncConfiguration(part, App.realmApp.CurrentUser);
 
-                _deviceissignrealm = await Realm.GetInstanceAsync(syncConfig);
+                signrealm = await Realm.GetInstanceAsync(syncConfig);
+                var newpage = signrealm.All<DisplaySign>().FirstOrDefault(t => t.OwnerId == App.realmApp.CurrentUser.Id);
+                if (newpage == null)
+                {
+                    Console.WriteLine("realm showing null");
+                    var transaction = signrealm.BeginWrite();
+                    try
+                    {
+                        var newsign = new DisplaySign
+                        {
+                            Displaytext = "UBER",
+                            Displaytext2 = "",
+                            Id = App.realmApp.CurrentUser.Id,
+                            OwnerId = App.realmApp.CurrentUser.Id
+                        };
+                        DisplayText = newsign.Displaytext;
+
+                        // Perform a write op...
+                        signrealm.Add(newsign);
+                        // Do other work that needs to be included in
+                        // this transaction
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Something went wrong; roll back the transaction
+                        transaction.Dispose();
+                    }
+
+
+
+
+                }
+
+                newpage.PropertyChanged += async (sender, eventArgs) =>  //changed from no async
+                {
+                    // pulls current value
+
+                    Debug.WriteLine("Pull value =" + newpage.Displaytext);
+                    NameofPage = newpage.Pagename;
+                    backcolor = newpage.Backgroundcolor;
+                    backcolor2 = newpage.Backgroundcolor2;
+                    displaytext = newpage.Displaytext;
+                    displaytext2 = newpage.Displaytext2;
+                    ridername = newpage.Ridername;
+                    textcolor = newpage.Textcolor;
+                    textcolor2 = newpage.Textcolor2;
+                    devicename = newpage.Devicename2;
+
+                    DisplayText = newpage.Displaytext;
+                };
+                DisplayText = newpage.Displaytext;
+                Console.WriteLine("Display Text = " + DisplayText);
+                Console.WriteLine("displaytext = " + displaytext);
+                // Later, when you no longer wish to receive notifications
+                //watch.Dispose();
+                
 
             }
 
-            var newpage = _deviceissignrealm.All<DisplaySign>().FirstOrDefault(t => t.Id == App.realmApp.CurrentUser.Id);
-            newpage.PropertyChanged += async (sender, eventArgs) =>  //changed from no async
-            {
-                // pulls current value
-
-                Debug.WriteLine("Pull value =" + newpage.Pagename);
-                NameofPage = newpage.Pagename;
-                backcolor = newpage.Backgroundcolor;
-                backcolor2 = newpage.Backgroundcolor2;
-                displaytext = newpage.Displaytext;
-                displaytext2 = newpage.Displaytext2;
-                ridername = newpage.Ridername;
-                textcolor = newpage.Textcolor;
-                textcolor2 = newpage.Textcolor2;
-                devicename = newpage.Devicename2;
-
-               
-            };
-
-
-            // Later, when you no longer wish to receive notifications
-            //watch.Dispose();
-
-
         }
 
-        public async Task ChangeSign()
-        {
-
-
-        }
+        
 
         private async Task ChangePage(string page)
         {
 
-            await AppShell.Current.GoToAsync("//" + page);
+            await Shell.Current.GoToAsync("//" + page);
         }
 
 
